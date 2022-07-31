1d53e4ff0742b171b60098d4407ee00bf5ccaa9a
regal-rahul <regalrahul777@gmail.com>
2022-07-31 17:41:38 +0530

    variable

---
+++ 02_Basics/01_Variables.js
@@ -0,0 +1,25 @@
+var fullName = "Rahul Kumar";
+
+// how to name variables
+
+// fullName - Most preferrable, Camel Case
+// full-name - better if ignored but can be used sometimes
+// full_name - Snake case - can be used 
+// FullName - Totally ignore
+
+var gitName = "Regal Rahul";
+
+var isLoggedIn = true; // boolean true/false
+// False/True is not considered boolean both are different.
+
+var loggedCount = 23;
+
+var paymentMode; // undefinded, data can be defined later on
+console.log(paymentMode); // now undefined
+paymentMode = "Credit Card" // data modified here
+console.log(paymentMode); // now prints the modified or inserted data
+
+console.log(loggedCount); // prints variable data
+console.log("loggedCount"); // prints passed string inside " xyz "
+
+// var if = 24; Resered keywords not allowed. editor will make you aware make sure to use a good editor.
