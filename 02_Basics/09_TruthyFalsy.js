30b7d275a967be354cb9b3b12f615100bc0c71e4
regal-rahul <regalrahul777@gmail.com>
2022-08-01 12:36:24 +0530

    truthy falsy values

---
+++ 02_Basics/09_TruthyFalsy.js
@@ -0,0 +1,22 @@
+// These are falshy values and anything apart from these are Truthy values
+// undefined
+// null
+// 0
+// '' Empty string
+// NaN not a number
+
+var user = '2';
+
+if (2 === user) {
+    console.log("Condition is true");
+}
+
+// if (2 == user) {
+//     console.log("Condition is true");
+// }
+
+// if (user) {
+//     console.log("Condition is true");
+// }
+
+// console.log("2" + 2);
