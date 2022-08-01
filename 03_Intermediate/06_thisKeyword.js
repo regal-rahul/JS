866fce0c343751d2afa5bfde4ce8d2d4d0ab4cbf
regal-rahul <regalrahul777@gmail.com>
2022-08-01 14:04:16 +0530

    accessing global context of engine

---
+++ 03_Intermediate/06_thisKeyword.js
@@ -0,0 +1,11 @@
+// TODO: part 1 for this keyword
+
+console.log(this); // returns empty scope in node engine but not in browser
+// this keyword gives access to global context of the engines
+var game = "cricket";
+
+// function sayName() {
+//     var name = "Rahul";
+//     console.log(this);
+// }
+// sayName();
