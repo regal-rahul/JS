bb2eabe4cdf7ad0a17ef1185812e88ebde65da2d
regal-rahul <regalrahul777@gmail.com>
2022-08-01 11:06:45 +0530

    if else

---
+++ 02_Basics/04_Conditionals.js
@@ -0,0 +1,14 @@
+var temperature;
+// Get the temperature data
+
+temperature = 39;
+
+if (temperature <= 20) {
+    console.log("It's very cold outside.");
+}
+
+if (temperature < 30) {
+    console.log("It's moderate outside.");
+} else {
+    console.log("It's too hot outside.");
+}
