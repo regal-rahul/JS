7f99012749a7c95407702db80902a271d250b62f
regal-rahul <regalrahul777@gmail.com>
2022-08-01 12:14:10 +0530

    ternary operator

---
+++ 02_Basics/07_ConditionalsSignup.js
@@ -0,0 +1,14 @@
+// Show user a signout button is he is authenticated
+// other wise show a option to Login/Signup
+
+var authenticated = true;
+// if (authenticated) {
+//     console.log("Show signout button");
+// } else {
+//     console.log("Show login option");
+// }
+
+// Must study this documentation
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators
+
+authenticated ? console.log("Signout Button") : console.log("Login");
