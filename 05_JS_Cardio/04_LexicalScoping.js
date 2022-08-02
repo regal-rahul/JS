6c66403b134d845b44950299216939891a465d7a
regal-rahul <regalrahul777@gmail.com>
2022-08-02 20:56:03 +0530

    lexical scoping

---
+++ 05_JS_Cardio/04_LexicalScoping.js
@@ -0,0 +1,11 @@
+function init() {
+    var firstName = "rahul";
+    function sayFirstName() {
+        console.log(firstName);
+    }
+    sayFirstName();
+}
+
+init();
+
+// console.log(firstName);
