88ac4d3f33a3184eaf5e4877ffbc455945e49790
regal-rahul <regalrahul777@gmail.com>
2022-08-02 14:15:19 +0530

    use of prototype

--- 05_JS_Cardio/01_Objects.js
+++ 05_JS_Cardio/01_Objects.js
@@ -6,8 +6,17 @@ var User = function (firstName, projectCount) {
     };
 };
 
+User.prototype.getFirstName = function () {
+    console.log(`Your firstname is : ${this.firstName}`);
+};
+
+
 var rahul = new User("rahul", 9);
-console.log(rahul);
+rahul.getProjectCount();
+rahul.getFirstName();
+// console.log(rahul);
 
 var regal = new User("regal", 7);
-console.log(regal);
+regal.getProjectCount();
+regal.getFirstName();
+// console.log(regal);
