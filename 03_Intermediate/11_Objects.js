aa14e314e0bcc3555bbe7d01d4b6ad9f869a48cd
regal-rahul <regalrahul777@gmail.com>
2022-08-02 10:49:15 +0530

    object assignment

--- 03_Intermediate/11_Objects.js
+++ 03_Intermediate/11_Objects.js
@@ -7,13 +7,26 @@ var user = {
     googleSignedIn: false
 };
 
-console.log(user.firstName);
+// console.log(user.firstName);
 // console.log(user[lastName]);
-console.log(user["lastName"]);
+// console.log(user["lastName"]);
 
-console.log(user.age); // before age update
-user.age = 23;
-console.log(user.age);
-console.log(user);
+// console.log(user.age); // before age update
+// user.age = 23;
+// console.log(user.age);
+// console.log(user);
 
-console.table(user);
+// console.table(user);
+
+var iPhone = {
+    modelNumber: "QWERTY12345",
+    modelName: "iPhone 15 pro",
+    launchYear: 2022,
+    price: 1999999,
+    color: ["black", "white", "red", "blue"],
+    storage: 128,
+    size: 6.75
+};
+
+console.table(iPhone);
+console.log(iPhone.color);
