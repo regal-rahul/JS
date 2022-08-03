35e8eb985e2288efa8364978eb3eb89b0a7e5674
regal-rahul <regalrahul777@gmail.com>
2022-08-03 14:10:29 +0530

    template literals

---
+++ 07_Advance/02_Strings.js
@@ -0,0 +1,17 @@
+var name = `raHul`;
+
+var fullName = `${name} kuMar`;
+console.log(fullName);
+
+// console.log(fullName.charAt(0));
+// console.log(fullName.charAt(2));
+// console.log(fullName.charAt(4));
+
+// console.log(fullName.endsWith("a"));
+// console.log(fullName.endsWith("?"));
+
+// console.log(fullName.includes("ahu"));
+// console.log(fullName.includes("ahur"));
+
+console.log(fullName.toUpperCase());
+console.log(fullName.toLowerCase());
