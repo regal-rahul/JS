ca0c67863dbbfa2ad30348fb0fd42bdcea213506
regal-rahul <regalrahul777@gmail.com>
2022-08-03 13:58:50 +0530

    var and let in scope

---
+++ 07_Advance/01_Scope.js
@@ -0,0 +1,11 @@
+// console.log(name);
+
+var name = "rahul";
+
+if (true) {
+    // var lastName = "kumar"; // Goes out of scope
+    // console.log(lastName);
+
+    let lastName = "kumar"; // Doesn't go out of scope
+}
+console.log(lastName);
