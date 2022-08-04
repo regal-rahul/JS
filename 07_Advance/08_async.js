3b9aef9279fadd6ddc73d1baf6749c409a568bbf
regal-rahul <regalrahul777@gmail.com>
2022-08-04 12:26:02 +0530

    event loops in js

---
+++ 07_Advance/08_async.js
@@ -0,0 +1,16 @@
+const uno = () => {
+    console.log("I am One");
+}
+const dos = () => {
+    setTimeout(() => {
+        console.log("Huurrrraaayyyyyy");
+    }, 3000);
+    console.log("I am Two");
+}
+const tres = () => {
+    console.log("I am Three");
+}
+
+uno();
+dos();
+tres();
