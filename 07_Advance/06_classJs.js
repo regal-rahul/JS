cd9f057587d8f21a10ecdda8bcd3c2837bf89398
regal-rahul <regalrahul777@gmail.com>
2022-08-04 12:09:00 +0530

    inheritance

--- 07_Advance/06_classJs.js
+++ 07_Advance/06_classJs.js
@@ -16,14 +16,38 @@ class User {
     getCourseList() {
         return this.#courseList;
     }
+    // static login() {
+    //     return "You are logged in"
+    // }
+    login() {
+        return "You are logged in"
+    }
 }
-
+class SubAdmin extends User{
+    constructor(name, email) {
+        super(name, email);
+    }
+    getAdminInfo() {
+        return "I am subadmin"
+    }
+    login() {
+        return "Login for admin only";
+    }
+}
 
 module.exports = User;
 
 
 const rock = new User ("rock", "rock@gmail.com");
-console.log(rock.getInfo());
+// console.log(rock.getInfo());
 rock.enrollCourse("Angular Bootcamp");
-console.log(rock.getCourseList());
-console.log(rock.courseList); // Will give undefined for private variables.
+// console.log(rock.getCourseList());
+// console.log(rock.courseList); // Will give undefined for private variables.
+
+
+
+const tom = new SubAdmin("tom", "tom@jerry.com");
+console.log(tom.getAdminInfo());
+console.log(tom.login());
+
+console.log(tom.getInfo());
